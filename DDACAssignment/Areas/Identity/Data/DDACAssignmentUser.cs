dbbc8fab7878f264ee66875933938f36fc465f0c
JAGDEESH SINGH A/L THARA SINGH <tp046012@mail.apu.edu.my>
2021-03-17 21:26:47 +0800

    Add project files.

---
+++ DDACAssignment/Areas/Identity/Data/DDACAssignmentUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace DDACAssignment.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the DDACAssignmentUser class
+    public class DDACAssignmentUser : IdentityUser
+    {
+    }
+}
---
+++ DDACAssignment/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using DDACAssignment.Areas.Identity.Data;
+using DDACAssignment.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(DDACAssignment.Areas.Identity.IdentityHostingStartup))]
+namespace DDACAssignment.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<DDACAssignmentContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("DDACAssignmentContextConnection")));
+
+                services.AddDefaultIdentity<DDACAssignmentUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<DDACAssignmentContext>();
+            });
+        }
+    }
+}
