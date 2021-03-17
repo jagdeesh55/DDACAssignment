4f80fbf37b4ce69775d27a0f898b191faa95ceb5
johnalvin04 <johnalvin04@gmail.com>
2021-03-17 22:30:50 +0800

    johntest

---
+++ DDACAssignment/Controllers/TableController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DDACAssignment.Controllers
+{
+    public class TableController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
